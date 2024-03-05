808a8627d387d7399ff1dbbd48ba8e369a3b1269
stnll <157781411+stnll@users.noreply.github.com>
2024-03-05 10:14:55 +0800

    Update Assignment 2: Matlab code.m

--- Assignment 2/Assignment 2: Matlab code.m
+++ Assignment 2/Assignment 2: Matlab code.m
@@ -6,13 +6,13 @@ close all
 syms s t
 
 F1 = ((1)/(s)*(s^2+2*s+s));
-f1 = laplace(F1);
+f1 = ilaplace(F1);
 pretty(f1)
 
 F2 = ((5*(s+2))/(s^2*(s+1)*(s+3)));
-f2 = laplace(F2);
+f2 = ilaplace(F2);
 pretty(f2)
 
 F3 = ((s^4+2*s^3+3*s^2+4*s+5)/(s*(s+1)));
-f3 = laplace(F3);
+f3 = ilaplace(F3);
 pretty(f3)
